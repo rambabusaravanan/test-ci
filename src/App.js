787f6354f07f15a3ff442f078ffb67d15b9d44e2
Andro Babu <rambabusaravanan@gmail.com>
2019-06-09 22:09:58 +0800

    changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn ReactJS
+          Start Learning ReactJS
         </a>
       </header>
     </div>
